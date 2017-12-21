d6f42b9348a65246d144e36fa5b2c9a6d1be01bf
madmazoku <madmazoku@gmail.com>
2017-12-21 15:57:09 +0300

    apply style

--- otus.lessons.05.01/otus.lessons.05.01.cpp
+++ otus.lessons.05.01/otus.lessons.05.01.cpp
@@ -18,110 +18,107 @@
 
 int main(int argc, char** argv)
 {
-	auto console = spdlog::stdout_logger_st("console");
-	console->info("Wellcome!");
+    auto console = spdlog::stdout_logger_st("console");
+    console->info("Wellcome!");
 
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		console->error("SDL_Init Error: {1}", SDL_GetError());
-		return 1;
-	}
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        console->error("SDL_Init Error: {1}", SDL_GetError());
+        return 1;
+    }
 
-	SDL_DisplayMode display_mode;
-	if (SDL_GetCurrentDisplayMode(0, &display_mode) != 0) {
-		console->error("SDL_CreateWindow Error: {1}", SDL_GetError());
-		SDL_Quit();
-		return 1;
-	}
+    SDL_DisplayMode display_mode;
+    if (SDL_GetCurrentDisplayMode(0, &display_mode) != 0) {
+        console->error("SDL_CreateWindow Error: {1}", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
 
-	long width = display_mode.w >> 1;
-	long height = display_mode.h >> 1;
+    long width = display_mode.w >> 1;
+    long height = display_mode.h >> 1;
 
-	Scene scene;
+    Scene scene;
 
-	SDL_Window *win = SDL_CreateWindow(
-		"Hellow World!",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		width,
-		height,
-		SDL_WINDOW_SHOWN
-	);
-	if (win == nullptr) {
-		console->error("SDL_CreateWindow Error: {1}", SDL_GetError());
-		SDL_Quit();
-		return 1;
-	}
+    SDL_Window *win = SDL_CreateWindow(
+                          "Hellow World!",
+                          SDL_WINDOWPOS_CENTERED,
+                          SDL_WINDOWPOS_CENTERED,
+                          width,
+                          height,
+                          SDL_WINDOW_SHOWN
+                      );
+    if (win == nullptr) {
+        console->error("SDL_CreateWindow Error: {1}", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
 
-	SDL_Surface *scr = SDL_GetWindowSurface(win);
-	SDL_Surface *img = SDL_CreateRGBSurface(0, scr->w, scr->h, 32, 0, 0, 0, 0);
+    SDL_Surface *scr = SDL_GetWindowSurface(win);
+    SDL_Surface *img = SDL_CreateRGBSurface(0, scr->w, scr->h, 32, 0, 0, 0, 0);
 
-	scene.init(width, height, 1);
+    scene.init(width, height, 9);
 
-	auto start = std::chrono::system_clock::now();
-	auto last = start;
+    auto start = std::chrono::system_clock::now();
+    auto last = start;
 
-	int count = 0;
-	int last_count = 0;
+    int count = 0;
+    int last_count = 0;
 
-	double time_step = 0.0;
-	
-	bool run = true;
-	while (run)
-	{
-		auto loop_start = std::chrono::system_clock::now();
-		++count;
+    double time_step = 0.0;
 
-		SDL_LockSurface(img);
+    bool run = true;
+    while (run) {
+        auto loop_start = std::chrono::system_clock::now();
+        ++count;
 
-		scene.render(img);
+        SDL_LockSurface(img);
 
-		SDL_UnlockSurface(img);
+        scene.process(img, time_step > 0.01 ? 0.01 : time_step);
 
-		SDL_BlitSurface(img, nullptr, scr, nullptr);
+        SDL_UnlockSurface(img);
 
-		SDL_UpdateWindowSurface(win);
+        SDL_BlitSurface(img, nullptr, scr, nullptr);
 
-		scene.step(time_step < 0.01 ? time_step : 0.01);
+        SDL_UpdateWindowSurface(win);
 
-		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT
-				|| event.type == SDL_KEYDOWN
-				|| event.type == SDL_KEYUP) {
-				run = false;
-			}
-		}
-		
-		auto end = std::chrono::system_clock::now();
-		std::chrono::duration<double> full_elapsed = end - start;
-		std::chrono::duration<double> last_elapsed = end - last;
-		std::chrono::duration<double> loop_elapsed = end - loop_start;
-		time_step = loop_elapsed.count();
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT
+                || event.type == SDL_KEYDOWN
+                || event.type == SDL_KEYUP) {
+                run = false;
+            }
+        }
 
-		if (!run || last_elapsed.count() >= 1) {
-			int frames = count - last_count;
-			double fps = ((double)frames) / last_elapsed.count();
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> full_elapsed = end - start;
+        std::chrono::duration<double> last_elapsed = end - last;
+        std::chrono::duration<double> loop_elapsed = end - loop_start;
+        time_step = loop_elapsed.count();
 
-			SDL_SetWindowTitle(win, ("Hello World! FPS: " + boost::lexical_cast<std::string>(fps)).c_str());
+        if (!run || last_elapsed.count() >= 1) {
+            int frames = count - last_count;
+            double fps = ((double)frames) / last_elapsed.count();
 
-			console->info("[{0} / {1}] fps: {2}; time_step: {3}", full_elapsed.count(), count, fps, time_step);
+            SDL_SetWindowTitle(win, ("Hello World! FPS: " + boost::lexical_cast<std::string>(fps)).c_str());
 
-			last = end;
-			last_count = count;
-		}
-		
-	}
+            console->info("[{0} / {1}] fps: {2}; time_step: {3}", full_elapsed.count(), count, fps, time_step);
 
-	SDL_FreeSurface(img);
-	SDL_FreeSurface(scr);
+            last = end;
+            last_count = count;
+        }
 
-	SDL_DestroyWindow(win);
-	SDL_Quit();
+    }
 
-	scene.done();
+    SDL_FreeSurface(img);
+    SDL_FreeSurface(scr);
 
-	console->info("Goodby!");
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 
-	return 0;
+    scene.done();
+
+    console->info("Goodby!");
+
+    return 0;
 }
 
--- otus.lessons.05.01/queue_processor.h
+++ otus.lessons.05.01/queue_processor.h
@@ -6,86 +6,98 @@
 #include <list>
 #include <queue>
 
-class QueueProcessor {
+class QueueProcessor
+{
 private:
-	std::list<std::thread> threads;
-	std::queue<std::function<void(void)> > tasks;
-	std::queue<std::function<void(void)> > finished_tasks;
-	unsigned int active_count;
-	bool terminate;
+    std::list<std::thread> threads;
+    std::queue<std::function<void(void)> > tasks;
+    std::queue<std::function<void(void)> > finished_tasks;
+    unsigned int active_count;
+    bool terminate;
 
-	std::mutex thread_mutex;
-	std::condition_variable thread_cv;
+    std::mutex thread_mutex;
+    std::condition_variable thread_cv;
 
 public:
-	QueueProcessor() : active_count(0), terminate(false) {};
+    QueueProcessor() : active_count(0), terminate(false) {};
 
-	void start(const unsigned int &thread_count = std::thread::hardware_concurrency()) {
-		while (threads.empty() || threads.size() < thread_count) {
-			threads.push_back(std::thread([&]() {
-				while (true) {
-					std::unique_lock<std::mutex> lock_thread(thread_mutex);
-					thread_cv.wait(lock_thread, [&]() { return !tasks.empty() || terminate; });
-					if (terminate)
-						break;
-					auto lambda = tasks.front();
-					tasks.pop();
-					++active_count;
-					lock_thread.unlock();
+    void start(const unsigned int &thread_count = std::thread::hardware_concurrency())
+    {
+        while (threads.empty() || threads.size() < thread_count) {
+            threads.push_back(std::thread([&]() {
+                while (true) {
+                    std::unique_lock<std::mutex> lock_thread(thread_mutex);
+                    thread_cv.wait(lock_thread, [&]() {
+                        return !tasks.empty() || terminate;
+                    });
+                    if (terminate)
+                        break;
+                    auto lambda = tasks.front();
+                    tasks.pop();
+                    ++active_count;
+                    lock_thread.unlock();
 
-					lambda();
+                    lambda();
 
-					lock_thread.lock();
-					finished_tasks.push(lambda);
-					--active_count;
-					if (active_count == 0 && tasks.empty()) {
-						thread_cv.notify_all();
-					}
-				}
-			}));
-		}
-	}
+                    lock_thread.lock();
+                    finished_tasks.push(lambda);
+                    --active_count;
+                    if (active_count == 0 && tasks.empty()) {
+                        thread_cv.notify_all();
+                    }
+                }
+            }));
+        }
+    }
 
-	void add(std::function<void(void)> lambda) {
-		std::lock_guard<std::mutex> lock_thread(thread_mutex);
-		tasks.push(lambda);
-	}
+    void add(std::function<void(void)> lambda)
+    {
+        std::lock_guard<std::mutex> lock_thread(thread_mutex);
+        tasks.push(lambda);
+    }
 
-	void rewind() {
-		std::lock_guard<std::mutex> lock_thread(thread_mutex);
-		while (!finished_tasks.empty()) {
-			auto lambda = finished_tasks.front();
-			finished_tasks.pop();
-			tasks.push(lambda);
-		}
-	}
+    void rewind()
+    {
+        std::lock_guard<std::mutex> lock_thread(thread_mutex);
+        while (!finished_tasks.empty()) {
+            auto lambda = finished_tasks.front();
+            finished_tasks.pop();
+            tasks.push(lambda);
+        }
+    }
 
-	void wait() {
-		std::unique_lock<std::mutex> lock_thread(thread_mutex);
-		while (!finished_tasks.empty())
-			finished_tasks.pop();
-		thread_cv.notify_all();
-		thread_cv.wait(lock_thread, [&]() { return tasks.empty() && active_count == 0; });
-	}
+    void wait()
+    {
+        std::unique_lock<std::mutex> lock_thread(thread_mutex);
+        while (!finished_tasks.empty())
+            finished_tasks.pop();
+        thread_cv.notify_all();
+        thread_cv.wait(lock_thread, [&]() {
+            return tasks.empty() && active_count == 0;
+        });
+    }
 
-	void stop() {
-		std::lock_guard<std::mutex> lock_thread(thread_mutex);
-		terminate = true;
-		thread_cv.notify_all();
-	}
+    void stop()
+    {
+        std::lock_guard<std::mutex> lock_thread(thread_mutex);
+        terminate = true;
+        thread_cv.notify_all();
+    }
 
-	void clear() {
-		std::lock_guard<std::mutex> lock_thread(thread_mutex);
-		while (!tasks.empty())
-			tasks.pop();
-		while (!finished_tasks.empty())
-			finished_tasks.pop();
-	}
+    void clear()
+    {
+        std::lock_guard<std::mutex> lock_thread(thread_mutex);
+        while (!tasks.empty())
+            tasks.pop();
+        while (!finished_tasks.empty())
+            finished_tasks.pop();
+    }
 
-	void join() {
-		for (auto &t : threads)
-			t.join();
+    void join()
+    {
+        for (auto &t : threads)
+            t.join();
 
-		threads.clear();
-	}
+        threads.clear();
+    }
 };
